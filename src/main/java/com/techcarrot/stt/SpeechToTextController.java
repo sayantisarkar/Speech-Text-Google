74e2420f15ee14b0985318e0217e61e4ae911026
DESKTOP-SJ\sayan <sayanti.sarkar@techcarrot.com>
2021-03-04 17:01:54 +0400

    Speech to text conversion  using Google cloud spring package

---
+++ src/main/java/com/techcarrot/stt/SpeechToTextController.java
@@ -0,0 +1,87 @@
+package com.techcarrot.stt;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.google.api.gax.longrunning.OperationFuture;
+import com.google.cloud.speech.v1.LongRunningRecognizeMetadata;
+import com.google.cloud.speech.v1.LongRunningRecognizeResponse;
+import com.google.cloud.speech.v1.RecognitionAudio;
+import com.google.cloud.speech.v1.RecognitionConfig;
+import com.google.cloud.speech.v1.RecognitionConfig.AudioEncoding;
+import com.google.cloud.speech.v1.SpeechClient;
+import com.google.cloud.speech.v1.SpeechRecognitionAlternative;
+import com.google.cloud.speech.v1.SpeechRecognitionResult;
+import com.google.protobuf.ByteString;
+import com.techcarrot.domain.Message;
+
+@RestController
+@RequestMapping("/speech")
+public class SpeechToTextController {
+
+	@ResponseBody
+	@GetMapping("/transcribe")
+	public ResponseEntity<Message> transcribeAudio() throws Exception {
+
+		/*
+		 * CredentialsProvider credentialsProvider;
+		 * 
+		 * SpeechSettings settings = null;
+		 * 
+		 * @Autowired public void setCredentialsProvider(CredentialsProvider
+		 * credentialsProvider) { this.credentialsProvider = credentialsProvider; }
+		 */
+
+		// Instantiates a client with GOOGLE_APPLICATION_CREDENTIALS
+		try (SpeechClient speech = SpeechClient.create()) {
+
+			// Setting audio configurations
+			RecognitionConfig config = RecognitionConfig.newBuilder().setEncoding(AudioEncoding.FLAC)
+					.setLanguageCode("en-US").setEnableAutomaticPunctuation(true).setEnableWordTimeOffsets(true)
+					.setModel("default").build();
+
+			// Local audio file Set
+			Path path = Paths.get("C:\\Users\\sayan\\Documents\\audio-file.flac");
+			byte[] data = Files.readAllBytes(path);
+			ByteString audioBytes = ByteString.copyFrom(data);
+			RecognitionAudio audio = RecognitionAudio.newBuilder().setContent(audioBytes).build();
+
+			// Cloud audio file set
+			// RecognitionAudio audio =
+			// RecognitionAudio.newBuilder().setUri("gs://sayanti/audio.flac").build();
+
+			// Use non-blocking call for getting file transcription
+			OperationFuture<LongRunningRecognizeResponse, LongRunningRecognizeMetadata> response = speech
+					.longRunningRecognizeAsync(config, audio);
+
+			while (!response.isDone()) {
+				System.out.println("Waiting for response...");
+				Thread.sleep(10000);
+			}
+
+			List<SpeechRecognitionResult> speechResults = response.get().getResultsList();
+
+			StringBuilder transcription = new StringBuilder();
+			for (SpeechRecognitionResult speechResult : speechResults) {
+				SpeechRecognitionAlternative alternative = speechResult.getAlternativesList().get(0);
+				transcription.append(alternative.getTranscript());
+			}
+			Message message = new Message();
+			message.setTranscript(transcription.toString());
+			//System.out.printf("Transcription: %s%n", transcription.toString());
+			return new ResponseEntity<Message>(message, HttpStatus.OK);
+
+		}
+
+	}
+
+}
---
+++ src/main/java/com/techcarrot/stt/SttGoogleApplication.java
@@ -0,0 +1,13 @@
+package com.techcarrot.stt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SttGoogleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SttGoogleApplication.class, args);
+	}
+
+}
